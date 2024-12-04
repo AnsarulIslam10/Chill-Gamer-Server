9f202387133dc09962ccd0784d8831e368897c8b
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-12-04 18:34:11 +0600

    my review detele method

--- index.js
+++ index.js
@@ -53,6 +53,15 @@ async function run() {
       res.send(result)
     })
 
+    // delete
+    app.delete('/review/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await reviewCollection.deleteOne(query);
+      res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
