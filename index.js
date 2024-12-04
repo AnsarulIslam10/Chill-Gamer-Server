e7392ccccaa16b57fce480ab7fb9c4f2946f6d9b
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-12-04 13:03:07 +0600

    find specific data from database by id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 5000;
@@ -30,6 +30,12 @@ async function run() {
     app.get('/reviews', async(req, res)=>{
       const cursor = reviewCollection.find();
       const result = await cursor.toArray();
+      res.send(result)
+    })
+    app.get('/review/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await reviewCollection.findOne(query);
       res.send(result)
     })
 
